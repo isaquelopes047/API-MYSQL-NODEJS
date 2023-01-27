ba5969f42294b6a6365fbaab1de4608fb3aa06c3
Isaque Lopes <isaque.lopes047@gmail.com>
2023-01-27 12:04:55 -0300

    add

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,24 +1,8 @@
-const conexao = require('../infraestrutura/conexao')
+const conexao = require('../infraestrutura/conexao');
 
 class Atendimento {
+    /* Cadatra um registro, e faz uma pequena validação */
     adiciona(atendimento, res) {
-
-        const nomeValido = atendimento.nomeFuncionario.length >= 5
-
-        const validacoes = [
-            {
-                nome: 'Nome',
-                valido: nomeValido,
-                mensagem: 'Nome deve possuir mais que 5 caracteres'
-            },
-        ]
-
-        const erros = validacoes.filter(campo => !campo.valido);
-        const existemErros = erros.length
-
-        if (existemErros) {
-            res.status(400).json(erros)
-        } else {
 
             const sql = 'INSERT INTO Atendimentos SET ?'
             conexao.query(sql, atendimento, (erro, resultados) => {
@@ -29,12 +13,73 @@ class Atendimento {
                     res.status(201).json(resultados)
                 }
             })
-        }
+        
     }
 
+    /* Todos os registros no banco */
     listar(res) {
         const sql = 'SELECT * FROM atendimentos'
         conexao.query(sql, (erro, resultados) => {
+            if(erro){
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultados)
+            }
+        })
+    }
+
+    /* Listar registro pelo seu primary key */
+    listarPorId(id, res) {
+        const sql = `SELECT * FROM atendimentos WHERE id = ${id}`
+        conexao.query(sql, (erro, resultados) => {
+            if(erro){
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultados)
+            }
+        })
+    }
+    
+    /* Listar registro por nomeFuncionario, GET em todos que tem registro */
+    listarPorFuncionario(nomeFuncionario, res) {
+        const sql = `SELECT * FROM atendimentos WHERE nomeFuncionario = '${nomeFuncionario}' ORDER BY id DESC`;
+        conexao.query(sql, (erro, resultados) => {
+            if(erro){
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultados)
+            }
+        })
+    }
+
+    /* Lista por data com dois parametros de data inicial e final */
+    listarPorData(dataInicio, dataFinal, res) {
+        const sql = `SELECT * FROM atendimentos WHERE dataEHora BETWEEN '${dataInicio}' and '${dataFinal}'`;
+        conexao.query(sql, (erro, resultados) => {
+            if(erro){
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultados)
+            }
+        })
+    }
+
+    /* Deletar um item por parametro */
+    DeletarPorId(id, res) {
+        const sql = `DELETE FROM atendimentos WHERE id = ${id}`
+        conexao.query(sql, (erro, resultados) => {
+            if(erro){
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultados)
+            }
+        })
+    }
+
+    /* Alterar um registro por parametro */
+    alterarRegistro(id, valores, res) {
+        const sql = 'UPDATE atendimentos SET ? WHERE id = ?'
+        conexao.query(sql, [valores, id], (erro, resultados) => {
             if(erro){
                 res.status(400).json(erro)
             } else {
