e252119126de579e99c3e5c233af6aa1c0503f15
Isaque Lopes <isaque.lopes047@gmail.com>
2023-01-31 08:46:55 -0300

    add

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const customExpress = require('./config/customExpress');
-const conexao = require('./infraestrutura/conexao');
 /* const Tabelas = require('./infraestrutura/tabelaAtendimento') */
 const PORT = process.env.PORT || 3200
+const app = customExpress();
 
-conexao.connect(erro => {
-
-    if(!erro) { 
-        const app = customExpress();
-        console.log('Conectado com o Bando de Dados com sucesso!')
-       /*  Tabelas.init(conexao) */
-        app.listen(PORT || process.env.PORT, () => { 
-            console.log(`Servidor rodando em: http://localhost:${PORT}`);
-        }); 
+/*  Tabelas.init(conexao) */
+app.listen(PORT || process.env.PORT, (error) => { 
+    if(!error){
+        console.log(`Servidor rodando em: http://localhost:${PORT}`);
+    } else {
+        console.log(`Erro ao iniciar`);
     }
-    else { console.log(erro) };
+}); 
+    
 
-});
+
+
